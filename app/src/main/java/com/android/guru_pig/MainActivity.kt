20645fcc226a49cfce2844d85ab25b200c84bd32
kimminseong777 <iceland0103zoro@gmail.com>
2023-01-27 15:26:27 +0900

    캘린더 내용 입력

--- app/src/main/java/com/android/guru_pig/MainActivity.kt
+++ app/src/main/java/com/android/guru_pig/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.android.guru_pig
 
-import android.content.Context
-import android.database.sqlite.SQLiteDatabase
-import android.database.sqlite.SQLiteOpenHelper
+import android.annotation.SuppressLint
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.EditText
+import android.widget.TextView
+
 import android.widget.FrameLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,10 +24,32 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.container)
     }
 
+    var userID: String = "userID"
+   /* lateinit var fname: String
+    lateinit var str: String
+    lateinit var calendarView: CalendarView
+    lateinit var updateBtn: Button
+    lateinit var deleteBtn:Button
+    lateinit var saveBtn:Button
+    lateinit var diaryTextView: TextView
+    lateinit var diaryContent:TextView
+    lateinit var contextEditText: EditText
+
+
+    @SuppressLint("MissingInflatedId")*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*// UI값 생성
+        calendarView=findViewById(R.id.calendarView)
+        diaryTextView=findViewById(R.id.diaryTextView)
+        saveBtn=findViewById(R.id.saveBtn)
+        deleteBtn=findViewById(R.id.deleteBtn)
+        updateBtn=findViewById(R.id.updateBtn)
+        diaryContent=findViewById(R.id.diaryContent)
+        contextEditText=findViewById(R.id.contextEditText)
+*/
         val bt_nav = findViewById<BottomNavigationView>(R.id.bottomnav)
 
         bt_nav.setOnItemSelectedListener { item ->
@@ -53,7 +83,31 @@ class MainActivity : AppCompatActivity() {
             )
             true
         }
-        bt_nav.selectedItemId = R.id.tab1
+
+
+
+       /* calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            diaryTextView.visibility = View.VISIBLE
+            saveBtn.visibility = View.VISIBLE
+            contextEditText.visibility = View.VISIBLE
+            diaryContent.visibility = View.INVISIBLE
+            updateBtn.visibility = View.INVISIBLE
+            deleteBtn.visibility = View.INVISIBLE
+            diaryTextView.text = String.format("%d / %d / %d", year, month + 1, dayOfMonth)
+            contextEditText.setText("")
+            checkDay(year, month, dayOfMonth, userID)
+        }
+
+        saveBtn.setOnClickListener {
+            saveDiary(fname)
+            contextEditText.visibility = View.INVISIBLE
+            saveBtn.visibility = View.INVISIBLE
+            updateBtn.visibility = View.VISIBLE
+            deleteBtn.visibility = View.VISIBLE
+            str = contextEditText.text.toString()
+            diaryContent.text = str
+            diaryContent.visibility = View.VISIBLE
+        }*/
     }
 
     private fun changeFragment(fragment: Fragment) {
@@ -62,4 +116,83 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.container, fragment)
             .commit()
     }
+
+    /*// 달력 내용 조회, 수정
+    fun checkDay(cYear: Int, cMonth: Int, cDay: Int, userID: String) {
+        //저장할 파일 이름설정
+        fname = "" + userID + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt"
+
+        var fileInputStream: FileInputStream
+        try {
+            fileInputStream = openFileInput(fname)
+            val fileData = ByteArray(fileInputStream.available())
+            fileInputStream.read(fileData)
+            fileInputStream.close()
+            str = String(fileData)
+            contextEditText.visibility = View.INVISIBLE
+            diaryContent.visibility = View.VISIBLE
+            diaryContent.text = str
+            saveBtn.visibility = View.INVISIBLE
+            updateBtn.visibility = View.VISIBLE
+            deleteBtn.visibility = View.VISIBLE
+            updateBtn.setOnClickListener {
+                contextEditText.visibility = View.VISIBLE
+                diaryContent.visibility = View.INVISIBLE
+                contextEditText.setText(str)
+                saveBtn.visibility = View.VISIBLE
+                updateBtn.visibility = View.INVISIBLE
+                deleteBtn.visibility = View.INVISIBLE
+                diaryContent.text = contextEditText.text
+            }
+            deleteBtn.setOnClickListener {
+                diaryContent.visibility = View.INVISIBLE
+                updateBtn.visibility = View.INVISIBLE
+                deleteBtn.visibility = View.INVISIBLE
+                contextEditText.setText("")
+                contextEditText.visibility = View.VISIBLE
+                saveBtn.visibility = View.VISIBLE
+                removeDiary(fname)
+            }
+            if (diaryContent.text == null) {
+                diaryContent.visibility = View.INVISIBLE
+                updateBtn.visibility = View.INVISIBLE
+                deleteBtn.visibility = View.INVISIBLE
+                diaryTextView.visibility = View.VISIBLE
+                saveBtn.visibility = View.VISIBLE
+                contextEditText.visibility = View.VISIBLE
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+    // 달력 내용 제거
+    @SuppressLint("WrongConstant")
+    fun removeDiary(readDay: String?) {
+        var fileOutputStream: FileOutputStream
+        try {
+            fileOutputStream = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
+            val content = ""
+            fileOutputStream.write(content.toByteArray())
+            fileOutputStream.close()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+    // 달력 내용 추가
+    @SuppressLint("WrongConstant")
+    fun saveDiary(readDay: String?) {
+        var fileOutputStream: FileOutputStream
+        try {
+            fileOutputStream = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
+            val content = contextEditText.text.toString()
+            fileOutputStream.write(content.toByteArray())
+            fileOutputStream.close()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+    }*/
 }
--- app/src/main/java/com/android/guru_pig/MonthFragment.kt
+++ app/src/main/java/com/android/guru_pig/MonthFragment.kt
@@ -1,12 +1,25 @@
 package com.android.guru_pig
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.Context.MODE_NO_LOCALIZED_COLLATORS
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
 import android.view.View
+import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CalendarView
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+
+import androidx.fragment.app.Fragment
 import java.util.*
+
 
 /**
  * A simple [Fragment] subclass.
@@ -17,18 +30,146 @@ class MonthFragment : Fragment() {
 
     private lateinit var calendar: CalendarView
 
+    var userID: String = "userID"
+    lateinit var fname: String
+    lateinit var str: String
+    lateinit var calendarView: CalendarView
+    lateinit var updateBtn: Button
+    lateinit var deleteBtn: Button
+    lateinit var saveBtn: Button
+    lateinit var diaryTextView: TextView
+    lateinit var diaryContent: TextView
+    lateinit var contextEditText: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /*setContentView(R.layout.activity_main)*/
+        calendarView= requireView().findViewById(R.id.calendarView)
+        diaryTextView=requireView().findViewById(R.id.diaryTextView)
+        saveBtn=requireView().findViewById(R.id.saveBtn)
+        deleteBtn=requireView().findViewById(R.id.deleteBtn)
+        updateBtn=requireView().findViewById(R.id.updateBtn)
+        diaryContent=requireView().findViewById(R.id.diaryContent)
+        contextEditText=requireView().findViewById(R.id.contextEditText)
+
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            diaryTextView.visibility = View.VISIBLE
+            saveBtn.visibility = View.VISIBLE
+            contextEditText.visibility = View.VISIBLE
+            diaryContent.visibility = View.INVISIBLE
+            updateBtn.visibility = View.INVISIBLE
+            deleteBtn.visibility = View.INVISIBLE
+            diaryTextView.text = String.format("%d / %d / %d", year, month + 1, dayOfMonth)
+            contextEditText.setText("")
+            checkDay(year, month, dayOfMonth, userID)
+        }
+
+        saveBtn.setOnClickListener {
+            saveDiary(fname)
+            contextEditText.visibility = View.INVISIBLE
+            saveBtn.visibility = View.INVISIBLE
+            updateBtn.visibility = View.VISIBLE
+            deleteBtn.visibility = View.VISIBLE
+            str = contextEditText.text.toString()
+            diaryContent.text = str
+            diaryContent.visibility = View.VISIBLE
+        }
+
+}
+
+
+
+
+/*override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+): View? {
+val view = inflater.inflate(R.layout.fragment_month, container, false)
+calendar = view.findViewById(R.id.calView)
+    // Inflate the layout for this fragment
+return view
+}*/
+    // 달력 내용 조회, 수정
+    fun checkDay(cYear: Int, cMonth: Int, cDay: Int, userID: String) {
+        //저장할 파일 이름설정
+        fname = "" + userID + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt"
+
+        var fileInputStream: FileInputStream
+        try {
+            fileInputStream = requireContext().openFileInput(fname)
+            val fileData = ByteArray(fileInputStream.available())
+            fileInputStream.read(fileData)
+            fileInputStream.close()
+            str = String(fileData)
+            contextEditText.visibility = View.INVISIBLE
+            diaryContent.visibility = View.VISIBLE
+            diaryContent.text = str
+            saveBtn.visibility = View.INVISIBLE
+            updateBtn.visibility = View.VISIBLE
+            deleteBtn.visibility = View.VISIBLE
+            updateBtn.setOnClickListener {
+                contextEditText.visibility = View.VISIBLE
+                diaryContent.visibility = View.INVISIBLE
+                contextEditText.setText(str)
+                saveBtn.visibility = View.VISIBLE
+                updateBtn.visibility = View.INVISIBLE
+                deleteBtn.visibility = View.INVISIBLE
+                diaryContent.text = contextEditText.text
+            }
+            deleteBtn.setOnClickListener {
+                diaryContent.visibility = View.INVISIBLE
+                updateBtn.visibility = View.INVISIBLE
+                deleteBtn.visibility = View.INVISIBLE
+                contextEditText.setText("")
+                contextEditText.visibility = View.VISIBLE
+                saveBtn.visibility = View.VISIBLE
+                //removeDiary(fname)
+            }
+            if (diaryContent.text == null) {
+                diaryContent.visibility = View.INVISIBLE
+                updateBtn.visibility = View.INVISIBLE
+                deleteBtn.visibility = View.INVISIBLE
+                diaryTextView.visibility = View.VISIBLE
+                saveBtn.visibility = View.VISIBLE
+                contextEditText.visibility = View.VISIBLE
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+    
+
+
+    // 달력 내용 제거
+    @SuppressLint("WrongConstant")
+    fun removeDiary(readDay: String?) {
+        var fileOutputStream: FileOutputStream
+        try {
+            fileOutputStream = requireContext().openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
+            val content = ""
+            fileOutputStream.write(content.toByteArray())
+            fileOutputStream.close()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_month, container, false)
-        calendar = view.findViewById(R.id.calView)
-            // Inflate the layout for this fragment
-        return view
+
+    // 달력 내용 추가
+    @SuppressLint("WrongConstant")
+    fun saveDiary(readDay: String?) {
+        var fileOutputStream: FileOutputStream
+        try {
+            fileOutputStream = requireContext().openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
+            val content = contextEditText.text.toString()
+            fileOutputStream.write(content.toByteArray())
+            fileOutputStream.close()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
     }
+
 
 }
